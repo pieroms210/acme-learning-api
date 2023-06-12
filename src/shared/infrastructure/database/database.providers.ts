7d1a1aafb49ac2ebab71f3c0d5b5215e06645bfa
Barbara Quezada <u202211800@upc.edu.pe>
2023-06-12 18:01:19 -0500

    feat: added database module with data source provider.

---
+++ src/shared/infrastructure/database/database.providers.ts
@@ -0,0 +1,23 @@
+import { async } from 'rxjs';
+import { DataSource } from 'typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+
+export const databaseProviders = [
+  {
+    provide: 'DATA_SOURCE',
+    useFactory: async () => {
+      const dataSource: DataSource = new DataSource({
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: 'root',
+        password: 'mysql',
+        database: 'lc',
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: true,
+        namingStrategy: new SnakeNamingStrategy(),
+      });
+      return dataSource.initialize();
+    },
+  },
+];
